f7966f0faf9bb16a434329338ba58288bc382b95
emre1n <emre1n@protonmail.com>
2025-07-29 13:23:08 +0300

    implement click button functionality with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { interval, map } from 'rxjs';
 
@@ -8,19 +15,29 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
   private destroyRef = inject(DestroyRef);
 
-  ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(map((value) => value * 2))
-      .subscribe({
-        next: (value) => {
-          console.log(value);
-        },
-      });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`);
     });
+  }
+
+  ngOnInit(): void {
+    // const subscription = interval(1000)
+    //   .pipe(map((value) => value * 2))
+    //   .subscribe({
+    //     next: (value) => {
+    //       console.log(value);
+    //     },
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
   }
 }
